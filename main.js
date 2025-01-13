05752435166c9dcbd726b688ad4ecdacc2cb9ffc
Widafitria <widafitria116@gmail.com>
2025-01-13 09:57:44 +0700

    update main.js

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
-import { log } from './logger'
-
-log('Import works!')
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js';
+import {
+  getFirestore,
+  collection,
+  doc,
+  getDocs,
+  getDoc,
+  addDoc,
+  deleteDoc,
+  updateDoc,
+  query,
+  orderBy
+} from 'https://www.gstatic.com/firebasejs/10.4.0/firebase-firestore.js';
